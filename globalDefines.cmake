354ff1ea687d83b710652e4460ba8124aa5184af
Guy Avraham <guyavrah1986@gmail.com>
2024-04-11 18:27:07 +0300

    adding global definitions for the compiler and linker flags for the covrage needs

---
+++ globalDefines.cmake
@@ -0,0 +1,6 @@
+
+# This file contains all sort of common definitions and settings to be used
+# in several places (other cmake files) within the project:
+# =============================================================================
+SET(unit_tests_link_options -fprofile-arcs -ftest-coverage)
+SET(unit_tests_compile_options -g -O0 -Wall -fprofile-arcs -ftest-coverage)
