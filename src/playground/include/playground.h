53738aa7e71418e12406c909ccdd3a57d65e8bfd
Guy Avraham <guyavrah1986@gmail.com>
2024-04-15 19:01:18 +0300

    adding playground function running flow

--- src/playground/include/playground.h
+++ src/playground/include/playground.h
@@ -2,4 +2,6 @@
 
 void runPlaygroundFunc(int argc, char** argv);
 
+void cpp11ThreadExample(int argc, char** argv);
+
 void sampleFuncToTestBoostLogger();
--- src/playground/playground.cpp
+++ src/playground/playground.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 #include "include/playground.h"
 
@@ -6,11 +7,35 @@ using namespace std;
 
 void runPlaygroundFunc(int argc, char** argv)
 {
-	sampleFuncToTestBoostLogger();
+	string funcName = "runPlaygroundFunc - ";
+    cout << funcName + "start" << endl;
+
+    // initialize the dictionary of functions:
+    typedef void (*funcPointer)(int, char**);
+    unordered_map<string, funcPointer> funcDict;
+      
+    // Add functions to the dictionary
+    funcDict["cpp11ThreadExample"] = &cpp11ThreadExample;
+
+    // extract the 2nd argument which indicates the function to run:
+    string funcToRunName = string(argv[2]);
+    auto it = funcDict.find(funcToRunName);
+    if (nullptr == it)
+    {
+        cout << funcName + "got an invalid name of playground function to run" << endl;
+        return;
+    }
+
+    cout << funcName + "about to run function:" << funcToRunName << endl;
+    (*it->second)(argc, argv);
+    cout << funcName + "back from function:" << funcToRunName << endl;
+    cout << funcName + "end" << endl;
 }
 
-void sampleFuncToTestBoostLogger()
+void cpp11ThreadExample(int argc, char** argv)
 {
-    cout << "sampleFuncToTestBoostLogger - start" << endl;
-    cout << "sampleFuncToTestBoostLogger - end" << endl;    
+    string funcName = "cpp11ThreadExample - ";
+    cout << funcName + "START" << endl;
+
+    cout << funcName + "END" << endl;
 }
